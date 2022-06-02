e1cba268088b2a5277a0c84b8e0c67860f80b1b2
icerow <nariamm.gonzalez@gmail.com>
2022-06-02 03:31:54 -0700

    Basic Configs

---
+++ .doom.d/custom.el
@@ -0,0 +1,20 @@
+;;; custom.el ---                                    -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  John Doe
+
+;; Author: John Doe <john@doe.com>
+;; Keywords:
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(ivy-gitlab vterm ssh-config-mode ssh password-store org-web-tools org-board neotree minimap gitlab dropbox dmenu beacon)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'customize-group 'disabled nil)
